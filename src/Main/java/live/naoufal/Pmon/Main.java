fed527ae4739762db123072e738ac8b3a83d0239
NaoufalAndichi <00313992@rijnijssel.nl>
2022-04-11 14:32:31 +0200

    commit

--- src/Main/java/live/naoufal/Pmon/Main.java
+++ src/Main/java/live/naoufal/Pmon/Main.java
@@ -5,6 +5,7 @@ import club.minnced.discord.webhook.WebhookClientBuilder;
 import club.minnced.discord.webhook.send.WebhookEmbed;
 import club.minnced.discord.webhook.send.WebhookEmbedBuilder;
 import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.model.Container;
 import com.github.dockerjava.core.DefaultDockerClientConfig;
 import com.github.dockerjava.core.DockerClientConfig;
 import com.github.dockerjava.core.DockerClientImpl;
@@ -12,6 +13,8 @@ import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
 import com.github.dockerjava.transport.DockerHttpClient;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -26,25 +29,32 @@ public class Main {
             .build();
     static DockerClient dockerClient = DockerClientImpl.getInstance(dockerClientConfig, dockerHttpClient);
 
-    public static void main(String[] args) {
-        Main.dockerClient.listContainersCmd().exec().forEach(container -> {
-            System.out.println(container.getId());
-        });
+    static List<String> containerNames = new ArrayList<>();
 
+    public static void main(String[] args) {
+        containerNames.add("amazing_roentgen");
         WebhookClientBuilder builder = new WebhookClientBuilder("https://discord.com/api/webhooks/951127407786614834/gUx-8ElAjs4JhEMmiaAaIphJSlDyh1f_t4BEGp5W1srJoyFME1p1xAXrQ0nUomnp-mej");
 
         builder.setWait(true);
-        WebhookClient client = WebhookClient.withUrl("https://discord.com/api/webhooks/951127407786614834/gUx-8ElAjs4JhEMmiaAaIphJSlDyh1f_t4BEGp5W1srJoyFME1p1xAXrQ0nUomnp-mej");
+        WebhookClient webhookClient = WebhookClient.withUrl("https://discord.com/api/webhooks/951127407786614834/gUx-8ElAjs4JhEMmiaAaIphJSlDyh1f_t4BEGp5W1srJoyFME1p1xAXrQ0nUomnp-mej");
 
+        for (Container container : dockerClient.listContainersCmd().exec()) {
+            webhookClient.send(String.join(", ", container.getNames()));
+        }
 
-//        this.client.listContainersCmd().exec()
-        client.send("**Message**");
+        for (Container container : Main.dockerClient.listContainersCmd().exec()) {
+                if ("running".equals(container.getState())) {
+                    continue;
+
+                }
+        }
+        webhookClient.send("**Message**");
 
         WebhookEmbed embed = new WebhookEmbedBuilder()
-                .setColor(0xFF00EE)
-                .setDescription("https://gitlab.rijnijssel.nl/00313992/javaopdracht")
+                .setColor(65280)
+                .setDescription("running :white_check_mark:")
                 .build();
-        client.send(embed)
+        webhookClient.send(embed)
                 .thenAccept((message) -> System.out.printf("Message with embed has been sent [%s]%n", message.getId()));
         {
         }
